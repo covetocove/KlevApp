7344c059b65da6124f260c1054d74c2f66a6912c
ChristianHansen <christianhshansen@gmail.com>
2015-05-04 17:10:56 -0400

    Added chunking functions

--- Klev/serial-test.py
+++ Klev/serial-test.py
@@ -6,11 +6,17 @@ import time
 MSG_START = "#"
 DELIMITER_SEQ = "|"
 DATA_DIR_PATH = ""
+MSG_END = "\x00"
+LINE_END = "\r\n" + MSG_END
+
+ACK = "ACK"
+MAX_LINE_LEN = 32
 
 tids_processed = set()
 cur_req_id = 1
 
 ser = serial.Serial(4, 57600, timeout=15)
+
 
 class serial_message(object):
 	def __init__(self, tid, nid, state):
@@ -35,7 +41,7 @@ def get_data():
 	send_req_and_get_resp("GET_DATA")
 
 def send_two_class_model():
-	send_file("SENDING_TWO_CLASS_MODEL", "KlevApp/ml/data_and_models\data_dummy_sample.model")
+	send_file("SENDING_TWO_CLASS_MODEL", "KlevApp/ml/data_and_models\data_toaster.model")
 
 def send_one_class_model():
 	send_file("SENDING_ONE_CLASS_MODEL", "KlevApp/ml/data_and_models\data_dummy_sample.one_class_model")
@@ -47,18 +53,54 @@ def send_file(header, file_name):
 	send_req_and_get_resp(header)
 	model_file = open(file_name)
 	for line in model_file:
-		send_req_and_get_resp(line[:-1] + "\r\n\0x00")
+		if(len(line) > MAX_LINE_LEN):
+			i = 0
+			end_ind = i + MAX_LINE_LEN
+			#we need to make sure not to send an empty line as the message
+			#Check if we can send the rest in < 2 messages
+			while(end_ind + MAX_LINE_LEN < len(line)):
+				send_req_wait_ack(line[i:end_ind] + MSG_END)		
+				i += MAX_LINE_LEN
+				end_ind = i + MAX_LINE_LEN
+			# Always send the max amount in the last message, so it
+			# can't be empty
+			# Send however much needed in the second to last to accomplish that
+			end_ind = len(line) - MAX_LINE_LEN
+			send_req_wait_ack(line[i:end_ind] + MSG_END)
+			i = end_ind
+			end_ind = len(line)
+			send_req_wait_ack(line[i:end_ind] + MSG_END)
+
+		else:
+			send_req_wait_ack(line[:-1] + LINE_END)
+		send_req_wait_ack(LINE_END)
 	model_file.close()
 
-def send_req_and_get_resp(msg_body):
+def send_req(msg_body):
 	global cur_req_id
 	global ser
-	message = MSG_START + str(cur_req_id) +  DELIMITER_SEQ + msg_body + "\r\n\x00"
+	message = MSG_START + str(cur_req_id) +  DELIMITER_SEQ + msg_body + LINE_END
 	ser.write(message)
 	print "--> " + message
 	cur_req_id += 1
 	ser.flush()
-	ser.readline() #need this to flush what we just sent?
+
+def send_req_wait_ack(msg_body):
+	send_req(msg_body)
+	read_line = ser.readline()
+	while read_line.find(ACK) < 0:
+		print "(ignoring {0})".format(read_line)
+		read_line = ser.readline()
+	print "<-- " + read_line
+
+def send_req_and_get_resp(msg_body):
+	send_req(msg_body)
+	read_line = ser.readline() #need this to flush what we just sent?
+	if read_line.find("Hello") > 0:
+		print "Tossing {0}".format(read_line)
+		read_line = ser.readline()
+
+	print "(first read {0})".format(read_line)
 	print "<-- "+  ser.readline()
 	return
 
